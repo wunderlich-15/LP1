d4a85e5be9e340b0f7a7924e98efdba2d720d822
wunderlich-15 <74203181+wunderlich-15@users.noreply.github.com>
2022-09-14 13:47:32 -0300

    Upload

---
+++ Agenda/src/agendajava/agenda/Agenda.java
@@ -0,0 +1,63 @@
+package agendajava.agenda;
+
+import java.io.FileWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.io.IOException;
+/**
+ *
+ * @author guilh
+ */
+public class Agenda {
+    private String nome;
+    private String numero;
+    private String email;
+    
+    
+    /*public void salvar(String nome, String numero, String email){
+        
+        try{
+            
+        }catch (IOException ex) {
+		// TODO Auto-generated catch block
+		Logger.getLogger(Agenda.class.getName()).log(Level.SEVERE, null, ex);
+	}
+    }*/
+    
+    public Agenda(){
+        
+    }
+    
+    public Agenda(String nome, String numero, String email){
+        this.nome = nome;
+        this.numero = numero;
+        this.email = email;
+        
+    }
+    
+    
+   //GETTERS E SETTERS
+    public String getNome(){
+        return nome;
+    }
+    
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    
+    public String getNumero(){
+        return numero;
+    }
+    
+    public void setNumero(String numero){
+        this.numero = numero;
+    }
+    
+    public String getEmail(){
+        return email;
+    }
+    
+    public void setEmail(String email){
+        this.email = email;
+    }
+}
